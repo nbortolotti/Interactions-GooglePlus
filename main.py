4162212e6c015b8c7e9c6b48f8c958298a94c249
Nicolas Bortolotti <nbortolotti@gmail.com>
2014-10-21 12:46:03 -0500

    Actualizacion para permitir modelar actividades en la integracion. Agragando componentes de visualizacion.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import json
+
 __autor__ = 'nickbortolotti'
 __licencia__ = 'Apache 2.0'
 
 import os
+import logging
 import sys
-import simplejson
 import jinja2
 import webapp2
 
@@ -14,7 +16,7 @@ from oauth2client.appengine import OAuth2DecoratorFromClientSecrets
 decorator = OAuth2DecoratorFromClientSecrets(os.path.join(os.path.dirname(__file__), 'client_secrets.json'),
                                              scope='https://www.googleapis.com/auth/plus.me')
 
-#Construccion del servicio de BigQuery
+#Construccion del servicio de GooglePlus
 servicio = build('plus', 'v1')
 
 #Variables del proyecto
@@ -26,21 +28,35 @@ Entorno_Jinja = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+
 class Panel(webapp2.RequestHandler):
     @decorator.oauth_required
     def get(self):
         http = decorator.http()
         try:
+            #lectura de informacion del usuario social
             usuario = servicio.people().get(userId='me').execute(http=http)
             nombre = usuario['displayName']
+
+            #lectura de informacion sobre un gplus activity
+            actividad = servicio.activities().get(activityId='z12vgx5plyjktgdskrjktprptpfui5mk1')\
+                .execute(http=http)
+            #obteniendo valores desde el servicio
+            a = actividad['url']
+
+            #lectura de informacion para las actividades
+            actividades = servicio.activities().list(userId='me', collection='public', maxResults='7')\
+                .execute(http=http)
 
             #Definicion de los datos para insertar en HTML. Jinja2
             plantilla_values = {
                  'nombre_usuario': nombre,
+                 'actividad': a,
+                 'actividades': actividades,
             }
 
             #Inferencia de la plantilla con el HTML correspondiente
-            template = Entorno_Jinja.get_template('index.html')
+            template = Entorno_Jinja.get_template('jqueryUI/index_002.html')
             self.response.write(template.render(plantilla_values))
 
         except:
